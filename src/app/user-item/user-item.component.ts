0c14d2ea4f2cea01f469fb41ea980150afd8556b
lbizik <lbizik@pccube.com>
2023-03-05 22:14:17 +0100

    a

--- src/app/user-item/user-item.component.ts
+++ src/app/user-item/user-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-item',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserItemComponent {
 
-  name: string;
+
+  @Input() name: string | undefined;
 
   constructor() {
-    this.name = 'Luisa';
+    
+  }
+
+  ngOnInit() {
+    
   }
 }
